cf08650d072044570520d7db38cf1d42888def96
orlando <acostavargasorlando99@gmail.com>
2020-11-06 18:26:37 -0500

    maquina1

---
+++ segundo_parcial/LogicaCambio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace segundo_parcial
+{
+    class LogicaCambio
+    {
+        //Encapsular los datos
+        private double numero1;
+        private double numero2;
+        private double resultado;
+
+        public double Numero1 { get => numero1; set => numero1 = value; }
+        public double Numero2 { get => numero2; set => numero2 = value; }
+        public double Resultado { get => resultado; set => resultado = value; }
+        public double Conversion(double num1, double num2, string op)
+        {
+            double resultado = double.NaN;
+            System.Console.WriteLine("Primer valor de la operación: {0}", num1);
+            System.Console.WriteLine("segundo valor de la operación: {0}", num2);
+        }
+    }
+}
---
+++ segundo_parcial/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace segundo_parcial
+{
+    class Program
+    {
+        private static LogicaCambio logica_Cambio;
+
+        public LogicaCambio LogicaCambio
+        {
+            get => default(LogicaCambio);
+            set
+            {
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program miAplicacion = new Program();
+            miAplicacion.ejecutarOperacionesCalculadora();
+        }
+        //Manejo de la pantalla
+        public void prepararPantalla()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Clear();
+        }
+        //Negocio pero en la capa de presentación (entrada, validación y llamado al servicio)
+        public void ejecutarOperacionesCalculadora()
+        {   
+            // datos de entrada 
+            // valor convercion 
+            string numInput1 = "";
+            // tasa 
+            string numInput2 = "";
+            // resultado
+            double resultado = 0;
+            logica_Cambio = new LogicaCambio();
+            // logica de menu op 
+            string op = "";
+            string op2 = "";
+
+            prepararPantalla();
+
+            Console.WriteLine("Bienvenido a la aplicacion de conversion \r");
+            // ingreso valor convercion 
+            Console.Write("\n Ingrese el valor de la moneda presione enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("No es una entrada válida.  Por favor ingrese un valor numérico: ");
+                numInput1 = Console.ReadLine();
+            }
+            
+            // menu de conversion
+            
+            do
+            {
+                Console.WriteLine("\t1 - Dolares Americanos");
+                Console.WriteLine("\t2 - Euros");
+                Console.WriteLine("\t3 - Yuanes (Renminbi)");
+                Console.WriteLine("\t4 - Yenes (Japon)");
+                Console.WriteLine("\t5 - Pesos Colombianos");             
+                Console.Write("Seleccione el tipo de moneda: ");
+                op = Console.ReadLine();
+            } while (!op.Equals("1") && !op.Equals("2") && !op.Equals("3") && !op.Equals("4") && !op.Equals("5"));
+            
+            // menu tipo de valor a convertir 
+
+            do
+            {
+                Console.WriteLine("\t Seleccione el tipo de valor a convertir");
+                Console.WriteLine("\t1 - Dolares Americanos");
+                Console.WriteLine("\t2 - Euros");
+                Console.WriteLine("\t3 - Yuanes (Renminbi)");
+                Console.WriteLine("\t4 - Yenes (Japon)");
+                Console.WriteLine("\t5 - Pesos Colombianos");
+                Console.Write("Seleccione el tipo de moneda: ");
+                op2 = Console.ReadLine();
+            } while (!op2.Equals("1") && !op2.Equals("2") && !op2.Equals("3") && !op2.Equals("4") && !op2.Equals("5"));
+            // ingreso de tasa de cambio 
+            Console.Write("\n Ingrese el valor de la tasa de cambio y presione enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2 = 0;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.Write("No es una entrada válida.  Por favor ingrese un valor numérico: ");
+                numInput2 = Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
